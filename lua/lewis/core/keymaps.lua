8a1b647f6d73cad18e1676dfd7fde14bbd7991a4
dpdlewis <lewis.wood@dpdgroup.co.uk>
2024-05-23 13:49:00 +0100

    tree focus and case toggle

--- lua/lewis/core/keymaps.lua
+++ lua/lewis/core/keymaps.lua
@@ -26,3 +26,5 @@ keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) 
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 keymap.set("n", "<leader>j", "<C->", { desc = "Return to previously open file" }) -- swap split with next split
+
+keymap.set("n", "<leader>cc", "~", { desc = "Change case of selection" }) --  move current buffer to new tab
